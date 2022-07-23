7a93bff757f833e12809c773c27766d28ba62947
Pavlukan <theremustbesomethingsmart@gmail.com>
2022-07-23 11:21:04 +0300

    Create a function which constructs user's desired grid
    The function takes the user's input and creates a new grid.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ for (let i = 0; i < 16; i++) { // create 16x16 grid
     }
 }
 
-const gridItems = document.querySelectorAll(".grid-block"); // create a node list of all the items with the class of grid-block
+let gridItems = document.querySelectorAll(".grid-block"); // create a node list of all the items with the class of grid-block
 for (let k = 0; k < gridItems.length; k++) { // loop through every item
     gridItems[k].addEventListener("mouseenter", () => { // when the mouse enters, change the color of the square to red
         gridItems[k].style.backgroundColor = "red"
@@ -19,4 +19,30 @@ for (let k = 0; k < gridItems.length; k++) { // loop through every item
 
 const button = document.querySelector("button"); // assign the variable button to the button in HTML
 button.addEventListener("click", selectGridSize); // when we click on the button, run the function selectGridSize()
+
+function selectGridSize() {
+    let gridSize = prompt("Enter the desired grid size: ");
+    +gridSize
+    if (gridSize > 100) {
+        return alert("Please enter a smaller number and try again");
+    } else {
+        for (gridItem of gridItems) { // remove every block from gridItems
+            gridItem.remove();
+        }
+
+        for (let p = 0; p < gridSize; p++) { // create new grid, using the user's input.
+            for (let f = 0; f < gridSize; f++) {
+                const block = document.createElement("div");
+                block.classList.add("grid-block");
+                grid.appendChild(block);
+                gridItems = document.querySelectorAll(".grid-block");
+                for (let k = 0; k < gridItems.length; k++) { // loop through every item
+                    gridItems[k].addEventListener("mouseenter", () => { // when the mouse enters, change the color of the square to red
+                        gridItems[k].style.backgroundColor = "red"
+                    });
+                }
+            }
+        }
+    }
+}
 
