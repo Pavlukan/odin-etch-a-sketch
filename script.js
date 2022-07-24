2c0fe0e70b1f0b5a47199c817f70b7eb8750ca5e
Pavlukan <theremustbesomethingsmart@gmail.com>
2022-07-24 10:27:09 +0300

    Make the grid change itself without altering the total allocated space

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
-const grid = document.querySelector(".sketchpad"); // assign the variable grid to the div with the class of sketchpad
+const grid = document.querySelector(".sketchpad"); 
 
-for (let i = 0; i < 16; i++) { // create 16x16 grid
+// The loop below creates 16x16 grid.
+for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
-        const block = document.createElement("div"); // create a div and assign it the variable of block
+        const block = document.createElement("div");
+        block.style.height = "37.5px";
+        block.style.width = "37.5px";
+        block.classList.add("grid-block"); 
 
-        block.classList.add("grid-block"); // add the class of grid-block to every grid item
-
-        grid.appendChild(block); // attach the block to the grid
+        grid.appendChild(block); 
     }
 }
 
-let gridItems = document.querySelectorAll(".grid-block"); // create a node list of all the items with the class of grid-block
-for (let k = 0; k < gridItems.length; k++) { // loop through every item
-    gridItems[k].addEventListener("mouseenter", () => { // when the mouse enters, change the color of the square to red
+// This code snippet creates the hover effect for every block.
+let gridItems = document.querySelectorAll(".grid-block");
+for (let k = 0; k < gridItems.length; k++) { 
+    gridItems[k].addEventListener("mouseenter", () => { 
         gridItems[k].style.backgroundColor = "red"
     });
 }
 
-const button = document.querySelector("button"); // assign the variable button to the button in HTML
-button.addEventListener("click", selectGridSize); // when we click on the button, run the function selectGridSize()
+// Make the function run when we click on the button
+const button = document.querySelector("button");
+button.addEventListener("click", selectGridSize); 
 
+// This function prompts the user to input grid size and creates a new grid, depending on the user's input
 function selectGridSize() {
     let gridSize = prompt("Enter the desired grid size: ");
     +gridSize
     if (gridSize > 100) {
         return alert("Please enter a smaller number and try again");
     } else {
+        blockSize = 600 / gridSize;
         for (gridItem of gridItems) { // remove every block from gridItems
             gridItem.remove();
         }
 
-        for (let p = 0; p < gridSize; p++) { // create new grid, using the user's input.
+        for (let p = 0; p < gridSize; p++) { 
             for (let f = 0; f < gridSize; f++) {
                 const block = document.createElement("div");
+                block.style.height = blockSize + "px";
+                block.style.width = blockSize + "px";
                 block.classList.add("grid-block");
                 grid.appendChild(block);
                 gridItems = document.querySelectorAll(".grid-block");
-                for (let k = 0; k < gridItems.length; k++) { // loop through every item
-                    gridItems[k].addEventListener("mouseenter", () => { // when the mouse enters, change the color of the square to red
+                for (let k = 0; k < gridItems.length; k++) { 
+                    gridItems[k].addEventListener("mouseenter", () => { 
                         gridItems[k].style.backgroundColor = "red"
                     });
                 }
